e92bb69ab9327f18ae07d9a15ed6ae63761cd67b
AlexRousseau92 <alex.rousseau92@gmail.com>
2021-12-21 08:23:24 +0100

    maj

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -7,19 +7,16 @@ const About = () => (
             <section className="container about">
                 <div className="about-left">
                     <div className="about-description">
-                        <h1 className='about-title'>Hello,</h1><br></br>
-                        <p className='about-text'> Après quelques années, <br></br>de navigation sur la Seine en tant que capitaine de bateau,
+                        <h1 className='about-title'>Hey,</h1>
+                        <p className='about-text'> Après quelques années, de navigation sur la Seine en tant que capitaine de bateau,
                             j'ai décidé de changer complètement de cap et de me tourner vers ce qui m'a toujours attiré
                             le développement web. J'ai suivi une formation de 6 mois <span className="about-js">"Développeur Web FullStack Javascript"</span> avec l'école
                             <a className="oclock" href="https://oclock.io" target="_blank"  rel="noreferrer"><span className="about-oclock"> O'Clock</span></a> afin d'apprendre les <span className='about-base'>"bases du métier"</span>.
                         </p>
-                            <ul className='about-skills'>
-                                <li className='about-skill'><i class="fab fa-html5"></i></li>
-                                <li className='about-skill'><i class="fab fa-css3-alt"></i></li>
-                                <li className='about-skill'><i class="fab fa-js"></i></li>
-                                <li className='about-skill'><i class="fab fa-react"></i></li>
-                                <li className='about-skill'><i class="fab fa-node"></i></li>
-                            </ul>
+                        <p className="about-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora iste dicta assumenda dolore maiores reprehenderit nostrum eaque, ipsam, tempore nam quia a, maxime pariatur ratione magni qui suscipit rem.
+                            
+                        </p>
                     </div>
                 </div>
                 <div className="about-right">
