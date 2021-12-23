c356651ac3f8e101073f6422024486e3fb85adb4
AlexRousseau92 <alex.rousseau92@gmail.com>
2021-12-23 17:56:50 +0100

    maj

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -5,22 +5,15 @@ import logoBlue from '../../Assets/AR_logo_blue.png';
 import logoWhite from '../../Assets/AR_logo_white.png';
 
 const Navbar = ({menuOpen, toggleMenu, routes, themeLight}) => {
-
-    if (themeLight) {
-        themeLight && <Link to="/"><img className="navbar-logo"src={logoBlue} alt=" le logo d'Alex" /></Link>
-    } else {
-        themeLight && <Link to="/"><img className="navbar-logo"src={logoWhite} alt=" le logo d'Alex" /></Link>
-    }
+    const lightMode = <Link to="/"><img className="navbar-logo"src={logoBlue} alt=" le logo d'Alex" /></Link>;
+    const darkMode = <Link to="/"><img className="navbar-logo"src={logoWhite} alt=" le logo d'Alex" /></Link>;
 
       return (
 
         <div className={menuOpen ? 'navbar show-nav' : 'navbar'}>
 
-            {themeLight ? (
-                <Link to="/"><img className="navbar-logo"src={logoBlue} alt=" le logo d'Alex" /></Link>
-            ) : (
-                <Link to="/"><img className="navbar-logo"src={logoWhite} alt=" le logo d'Alex" /></Link>
-            )}
+            {themeLight ? lightMode : darkMode}
+
             <div  className="navbar-links">
                 {
                     routes.map(({label, route, id, classCss})=> (
