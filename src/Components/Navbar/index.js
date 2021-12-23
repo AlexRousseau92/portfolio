2e8d838ef8864661cf51ad825cc5f8d6a422ec29
AlexRousseau92 <alex.rousseau92@gmail.com>
2021-12-23 17:49:18 +0100

    maj darkmod

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './style.scss';
-import logo from '../../Assets/AR_logo_blue.png';
+import logoBlue from '../../Assets/AR_logo_blue.png';
+import logoWhite from '../../Assets/AR_logo_white.png';
 
 const Navbar = ({menuOpen, toggleMenu, routes, themeLight}) => {
+
+    if (themeLight) {
+        themeLight && <Link to="/"><img className="navbar-logo"src={logoBlue} alt=" le logo d'Alex" /></Link>
+    } else {
+        themeLight && <Link to="/"><img className="navbar-logo"src={logoWhite} alt=" le logo d'Alex" /></Link>
+    }
 
       return (
 
         <div className={menuOpen ? 'navbar show-nav' : 'navbar'}>
-            <Link to="/"><img className="navbar-logo"src={logo} alt=" le logo d'Alex" /></Link>
+
+            {themeLight ? (
+                <Link to="/"><img className="navbar-logo"src={logoBlue} alt=" le logo d'Alex" /></Link>
+            ) : (
+                <Link to="/"><img className="navbar-logo"src={logoWhite} alt=" le logo d'Alex" /></Link>
+            )}
             <div  className="navbar-links">
                 {
                     routes.map(({label, route, id, classCss})=> (
